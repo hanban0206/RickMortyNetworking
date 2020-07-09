d2e36d62ddbcb7408077bd4a680d1777c8f25dd6
Shawn Gee <shawngee@gmail.com>
2020-07-09 01:34:55 -0400

    Basic collection view set up

---
+++ RickMortyNetworking/RickMortyNetworking/CharacterCollectionViewController.swift
@@ -0,0 +1,86 @@
+//
+//  CharacterCollectionViewController.swift
+//  RickMortyNetworking
+//
+//  Created by Shawn Gee on 7/9/20.
+//  Copyright © 2020 Swift Student. All rights reserved.
+//
+
+import UIKit
+
+
+class CharacterCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let searchController = UISearchController()
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search for a character"
+        searchController.searchBar.delegate = self
+        self.navigationItem.searchController = searchController
+
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CharacterCell", for: indexPath)
+    
+        cell.backgroundColor = .systemGray4
+        cell.layer.cornerRadius = 8
+    
+        return cell
+    }
+
+}
+
+private let padding: CGFloat = 10
+private let numRows: CGFloat = 2
+
+extension CharacterCollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let totalPadding = padding * (numRows + 1)
+        let width = (collectionView.frame.width - totalPadding) / numRows
+        
+        return CGSize(width: width, height: width)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return padding
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+    }
+}
+
+extension CharacterCollectionViewController: UISearchResultsUpdating, UISearchBarDelegate{
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("Fetch the search results")
+    }
+    
+}
